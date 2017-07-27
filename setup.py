ff6cc9fc56ec3c3b172afddb928ec66cdb8ffb80
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-07-26 21:01:22 -0300

    https://pypi.python.org/pypi/automated/1.0.0

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
    name='automated',
    version='1.0.0',
    description='A automated module',
+   long_description=long_description,
+   license='MIT',
+   python_requires='>=3',
+   url='https://github.com/gabrielfern/automated-leda-tasks',
    author='Gabriel Fernandes',
    author_email='gabrielfernndss@gmail.com',
    packages=['automated'],
