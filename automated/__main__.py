2fc08ae4f825e8ca4f8300364895b184a9ae064a
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-08-03 18:32:41 -0300

    organizando

--- automated/__main__.py
+++ automated/__main__.py
@@ -98,44 +98,40 @@ def main():
             main()
         else:
             if len(sys.argv) > 1:
-                options = ('help', 'info', 'reset', 'hora', 'hoje', 'cronograma')
                 command = sys.argv[1]
-                if command in options:
-                    options = {'info': get_personal_info,
-                               'reset': autoit.reset_config,
-                               'hora': retrievedata.get_hora_atual,
-                               'hoje': retrievedata.get_roteiro_today,
-                               'cronograma': retrievedata.req_crono}
-                    if command == 'info' or command == 'hora':
-                        print(options[command]())
-                    elif command == 'reset':
-                        print(options[command](HERE))
-                    elif command == 'cronograma' or command == 'hoje':
-                        pprint(options[command](get_personal_info()['turma']))
-                    elif command == 'help':
-                        print('Como usar:',
-                                    '\n\tEh recomendado instalar usando o pip do python (utilitario que ja vem com o python)',
-                                    '\n\tpois possibilita rodar com um so comando de qualquer diretorio sem se preocupar em mudar de pasta',
-                                        '\n\tPara instalar:',
-                                            '\n\t\tpip install --user automated-leda-tasks',
-                                            '\n\t\t(necessario ter o pacote na pasta atual)',
-                                        '\n\tPara rodar:',
-                                            '\n\t\tpython -m automated [opcional]',
-                                            '\n\t\t(so eh necessario rodar desta forma para ele fazer todo o trabalho)',
-                                    '\n\tTambem eh possivel utilizar cada funcao independente importando do seu script/interpretador python',
-                                        '\n\t\tfrom automated import [autoit | retrievedata]',
-                                    '\n\tPossui diversos utilitarios interessantes, recomendado ver o codigo fonte de cada modulo',
-                                '\nOpcionais:',
-                                    '\n\thelp::          mostra essa mensagem',
-                                    '\n\treset::         reseta configuracoes',
-                                    '\n\tinfo::          exibe informacoes configuradas',
-                                    '\n\thora::          exibe hora segundo o servidor',
-                                    '\n\thoje::          mostra se existe roteiro para hoje',
-                                    '\n\tcronograma::    exibe as datas para todos os roteiros do periodo'
-                            )
+
+                if command == 'info':
+                    pprint(get_personal_info())
+                elif command == 'hora':
+                    print(retrievedata.get_hora_atual())
+                elif command == 'reset':
+                    print(autoit.reset_config(HERE))
+                elif command == 'hoje':
+                    print(retrievedata.get_roteiro_today(get_personal_info()['turma']))
+                elif command == 'cronograma':
+                    pprint(retrievedata.req_crono(get_personal_info()['turma']))
                 else:
-                    print('Opcional %s nao valido, ' %command,
-                          '\nConfira em "python -m automated help" os opcionais validos')
+                    print('Como usar:',
+                                '\n\tEh recomendado instalar usando o pip do python (utilitario que ja vem com o python)',
+                                '\n\tpois possibilita rodar com um so comando de qualquer diretorio sem se preocupar em mudar de pasta',
+                                    '\n\tPara instalar:',
+                                        '\n\t\tpip install --user automated-leda-tasks',
+                                        '\n\t\t(necessario ter o pacote na pasta atual)',
+                                    '\n\tPara rodar:',
+                                        '\n\t\tpython -m automated [opcional]',
+                                        '\n\t\t(so eh necessario rodar desta forma para ele fazer todo o trabalho)',
+                                '\n\tTambem eh possivel utilizar cada funcao independente importando do seu script/interpretador python',
+                                    '\n\t\tfrom automated import [autoit | retrievedata]',
+                                '\n\tPossui diversos utilitarios interessantes, recomendado ver o codigo fonte de cada modulo',
+                            '\nOpcionais:',
+                                '\n\thelp::          mostra essa mensagem',
+                                '\n\thora::          exibe hora segundo o servidor',
+                                '\n\thoje::          mostra se existe roteiro para hoje',
+                                '\n\tinfo::          exibe informacoes configuradas',
+                                '\n\treset::         reseta configuracoes',
+                                '\n\tcronograma::    exibe as datas para todos os roteiros do periodo'
+                        )
+
             else:
                 data = get_personal_info()
                 roteiro = retrievedata.match_roteiro(data['turma'])
