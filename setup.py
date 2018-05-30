cb81974e804515c8aa7ec556fd4a14d1db4db94f
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-05-29 22:42:30 -0300

    v1.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
    name='automated',
-   version='1.2.0',
+   version='1.2.1',
    description='Automatizador de tarefas - LEDA',
    license='MIT',
    classifiers=[
